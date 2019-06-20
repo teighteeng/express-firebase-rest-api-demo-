c38944983f5b923991b0194ee6c03c1ea6160c53
Thanapong Yingyong <thanapongtle@gmail.com>
2019-06-20 17:35:40 +0700

    👨🏻‍💻test req: change now.js, fix server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 
+const port = process.env.PORT || 3000
+
 app = express()
+app.use(express.json())
+
+app.listen(port, err => {
+  if (err) throw err
+  console.log(`> Ready On Server http://localhost:${port}`)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello')
